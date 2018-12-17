dafbf5f003aec39346426526a2f4733fb5559a66
brb228332 <45010061+brb228332@users.noreply.github.com>
2018-12-17 20:46:10 +0300

    Update and rename README.md to l4/src/Main.java

---
+++ l4/src/Main.java
@@ -0,0 +1,9 @@
+# wtf
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Second Commit");
+        System.out.println("Changes on GitHub");
+    }
+}
